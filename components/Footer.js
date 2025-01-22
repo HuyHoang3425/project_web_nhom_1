dc0e46c3b84017a2b09c7097e69cd7a76690c21d
bui huy hoang <buihoang34252@gmail.com>
2025-01-23 01:05:47 +0700

    complete rent

---
+++ components/Footer.js
@@ -0,0 +1,27 @@
+export const Footer = `
+ <footer>
+        <img class="car-footer" src="../../assets/images/home/car-footer.png" alt="car-footer">
+        <ul class="social">
+            <li><i class="fa-brands fa-facebook-f"></i></li>
+            <li><i class="fa-brands fa-x-twitter"></i></li>
+            <li><i class="fa-brands fa-whatsapp"></i></li>
+            <li><i class="fa-brands fa-skype"></i></li>
+        </ul>
+        <svg class="svg-5" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1600 406">
+            <path style="fill:rgba(167,167,167,0.25)" d="M1608.03,417H-1.709L-3.493,267.327c76.974,52.083,173.369,106.4,262.948,114.816,82.7,7.772,175.138-15.857,175.138-15.857,296.27-61.5,450.55-323.851,772.167-357.194,146.02-12.5,299.07,11.613,401.05,33.813Z"></path>
+            <path style="fill:#ffca08" d="M-2.25,405.5H1607.5l1-248.5S1498.97,67.09,1199,82.5C952.453,84.455,717.723,320.6,424,369c0,0-91,17-172,11-87.435-6.475-177.172-39.147-255.516-77.726Z"></path>
+            <path style="fill:#1c1601" d="M-5.143,340.337C87.619,364.359,200.042,380.486,309,381c298.183,1.4,631.464-245.093,878-242,244.99,3.074,420.5,129,420.5,129V410H-4.75Z"></path>
+            </svg>
+
+            <div class="gap" style="background-color: #1c1601; margin-top: -10px;"></div>
+            <div class="gap" style="background-color: #1c1601;"></div>
+
+            <div class="card">
+                <img src="../../assets/images/home/mail-footer.png" alt="">
+                <div class="title">
+                    <span>Need a Car? Contact us now with </span><span>+1 (818) 123 456 789</span>
+                </div>
+            </div>
+            <img src="../../assets/images/home/logo-footer.png" alt="">
+            <p>Copyright © 2025 Xtra Theme. All Rights Reserved. Powered by WordPress.</p>
+     </footer>`
---
+++ components/header.js
@@ -0,0 +1,15 @@
+export const header =`
+<img src="../../assets/images/home/car-rental-logo.png" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Rent</a></li>
+                <li><a href="#">Locations</a></li>
+                <li><a href="#">Gold Rewards</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Pages</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <button class="btn">Request a Car</button>
+` 
